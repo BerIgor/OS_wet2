c0a947df72839a834ca7be0a076b656085c2bcb9
Olga Zendel <olgazendel@gmail.com>
2016-05-10 09:49:41 +0300

    changed things

--- account.h
+++ account.h
@@ -1,6 +1,8 @@
 #ifndef ACCOUNT_H_
 #define ACCOUNT_H_
 
+#include <pthread.h>
+
 using namespace std;
 
 class Account {
@@ -8,10 +10,16 @@ public:
 	int number;
 	int password;
 	int balance;
+	pthread_mutex_t writeLock;
+	pthread_mutex_t readLock;
+	int read_cnt;
 
-	//TODO: change to include mutex
 	Account(int _accountNumber, int _accountPassword, int _accountBalance) :
-			number(_accountNumber), password(_accountPassword), balance(_accountBalance){}
+			number(_accountNumber), password(_accountPassword), balance(_accountBalance){
+		read_cnt=0;
+		pthread_mutex_init(&writeLock, NULL);
+		pthread_mutex_init(&readLock, NULL);
+	}
 
 
 
